c64a1e6fdb4c838a0c2a2cb2a27af8357e9627d5
Garrettow <=>
2021-01-10 13:20:07 -0500

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {React, useState} from 'react';
+import {React, useState, useEffect} from 'react';
 import './App.sass';
 import Key from './Components/Key';
 
@@ -13,17 +13,32 @@ const App = () => {
 
 	const [keysGuessed, setKeysGuessed] = useState([]);
 	const [currentWord, setCurrentWord] = useState('test');
+	const [guessCount, setGuessCount] = useState(5);
+
+	const load = () => {
+		setKeysGuessed([]);
+		setGuessCount(5);
+		setCurrentWord(words.maps.payload[Math.floor(Math.random() * (words.maps.payload.length))])
+	}
 
 	const guessHandler = x => {
+		setGuessCount(guessCount - 1);
 		setKeysGuessed(keysGuessed.concat(x));
 	}
 
+	useEffect(() => {
+		load();
+	}, [])
+
 	return (
     	<div className="App">
-			<p>{keysGuessed}</p>
 			{
 				alphabet.map(x => <Key disabled={keysGuessed.includes(x) ? true : false} index={x} guessHandler={guessHandler}>{x}</Key>)
 			}
+			<p>{guessCount} Guesses remaining!</p>
+			<p>{keysGuessed}</p>
+			<p>Current Word: {currentWord}</p>
+			<button onClick={() => load()}>New Word</button>
     	</div>
   	);
 }
